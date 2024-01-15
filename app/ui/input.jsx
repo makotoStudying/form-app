a4e5126ede5b59c5f06938cb1c9db6ed617053a5
Makoto <makoto19950928@outlook.jp>
2024-01-16 00:53:07 +0900

    update

--- app/ui/input.jsx
+++ app/ui/input.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 const Input = ({ label, id, type, value, onChange, ...props }) => (
-  <>
+  <div>
     <label htmlFor={id}>{label}</label>
     <input id={id} type={type} value={value} onChange={onChange} {...props} />
-  </>
+  </div>
 );
 
 export default Input;
