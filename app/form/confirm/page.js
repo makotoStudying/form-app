1b23485bc4b341fed6f6d262e82df702cdc4825d
Makoto <makoto19950928@outlook.jp>
2024-01-22 20:33:28 +0900

    update

--- app/form/confirm/page.js
+++ app/form/confirm/page.js
@@ -1,4 +1,3 @@
-"use client";
 import { useRouter } from "next/navigation";
 import { useDataContext } from "../../context/dataContext";
 import Confirm from "../../ui/confirm";
@@ -7,7 +6,7 @@ import { fetchForm } from "../../lib/actions";
 export default function Page() {
   const router = useRouter();
   const { data, setData } = useDataContext();
-
+  ("use server");
   const action = async () => {
     await fetchForm(data);
     router.push("/form/thanks");
