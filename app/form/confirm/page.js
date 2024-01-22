74297ec89e7b7351687a88c441f4d17daa0f98fb
Makoto <makoto19950928@outlook.jp>
2024-01-22 20:51:16 +0900

    update

--- app/form/confirm/page.js
+++ app/form/confirm/page.js
@@ -7,7 +7,8 @@ import { fetchForm } from "../../lib/actions";
 export default function Page() {
   const router = useRouter();
   const { data, setData } = useDataContext();
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     await fetchForm(data);
     router.push("/form/thanks");
   };
